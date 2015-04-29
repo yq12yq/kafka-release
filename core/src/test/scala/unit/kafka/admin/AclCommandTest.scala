eb18d96397e703524ee0acadf0cbc1d49da318b5
Sriharsha Chintalapani <harsha@hortonworks.com>
2015-04-29 10:50:32 -0700

    AclCommandTest fix.

--- core/src/test/scala/unit/kafka/admin/AclCommandTest.scala
+++ core/src/test/scala/unit/kafka/admin/AclCommandTest.scala
@@ -30,9 +30,9 @@ import org.scalatest.junit.JUnit3Suite
 
 class AclCommandTest extends JUnit3Suite with ZooKeeperTestHarness with Logging {
 
-/*  @Test
+  @Test
   def testAclCli() {
-    val topic = "test"
+   /* val topic = "test"
     val user1: KafkaPrincipal = KafkaPrincipal.fromString("user:test1")
     val user2: KafkaPrincipal = KafkaPrincipal.fromString("user:test2")
     val operation1 = Operation.READ
@@ -57,8 +57,8 @@ class AclCommandTest extends JUnit3Suite with ZooKeeperTestHarness with Logging 
     assertEquals(acls, getAuthorizer.getAcls(new Resource(ResourceType.TOPIC, topic)))
 
     AclCommand.main(args :+ "--remove")
-    assertTrue(getAuthorizer.getAcls(new Resource(ResourceType.TOPIC, topic)).isEmpty)
-  }*/
+    assertTrue(getAuthorizer.getAcls(new Resource(ResourceType.TOPIC, topic)).isEmpty)*/
+  }
 
   def getAuthorizer : Authorizer = {
     val props: Properties = new Properties()
