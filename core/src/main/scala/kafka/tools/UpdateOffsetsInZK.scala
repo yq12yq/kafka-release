9224fd9b1b42609a26a5c5e194da438eaa9b4080
Parth Brahmbhatt <brahmbhatt.parth@gmail.com>
2016-01-18 17:50:06 -0800

    BUG-51095: UpdateOffsetsInZK should read security protocol from the consumer config.

--- core/src/main/scala/kafka/tools/UpdateOffsetsInZK.scala
+++ core/src/main/scala/kafka/tools/UpdateOffsetsInZK.scala
@@ -65,10 +65,11 @@ object UpdateOffsetsInZK {
           "getOffsetsBefore request")
       }
 
+      val securityProtocol = SecurityProtocol.valueOf(config.securityProtocol)
       zkUtils.getBrokerInfo(broker) match {
         case Some(brokerInfo) =>
-          val consumer = new SimpleConsumer(brokerInfo.getBrokerEndPoint(SecurityProtocol.PLAINTEXT).host,
-                                            brokerInfo.getBrokerEndPoint(SecurityProtocol.PLAINTEXT).port,
+          val consumer = new SimpleConsumer(brokerInfo.getBrokerEndPoint(securityProtocol).host,
+                                            brokerInfo.getBrokerEndPoint(securityProtocol).port,
                                             10000, 100 * 1024, "UpdateOffsetsInZk")
           val topicAndPartition = TopicAndPartition(topic, partition)
           val request = OffsetRequest(Map(topicAndPartition -> PartitionOffsetRequestInfo(offsetOption, 1)))
