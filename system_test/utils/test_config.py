649cc78f627dac854f8b39a635ed16108413015d
Raghav Kumar Gautam <raghavgautam@users.noreply.github.com>
2016-03-22 14:18:09 -0700

    the kinit command must end with ;

--- system_test/utils/test_config.py
+++ system_test/utils/test_config.py
@@ -8,7 +8,7 @@ class TestConfig(object):
     @classmethod
     def get_kinit_cmd(self, secureMode):
         if not secureMode:
-            return "echo"
+            return "echo;"
         keytab = "/home/HDINSIGHT/hrt_qa/hadoopqa/keytabs/hrt_qa.headless.keytab"
         principle = "hrt_qa@HDINSIGHT.NET"
         kinit_cmd = "/usr/bin/kinit  -k -t %s %s;" % (keytab, principle)  # must end with ;
