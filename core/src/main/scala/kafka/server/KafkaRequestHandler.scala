7f8ddc0d14509e41e701863b9c5163d719cfdfb2
Satish Duggana <satishd@apache.org>
2018-06-13 20:14:34 +0530

    MINOR Added scheduled task to cleanup cache to consider producers as inactive in determinitic manner
    Change-Id: I3137794fe830ca74f8b7d5e11d8e00534d99160b

--- core/src/main/scala/kafka/server/KafkaRequestHandler.scala
+++ core/src/main/scala/kafka/server/KafkaRequestHandler.scala
@@ -19,14 +19,13 @@ package kafka.server
 
 import java.util
 import java.util.Objects
-
-import kafka.network._
-import kafka.utils._
-import kafka.metrics.KafkaMetricsGroup
-import java.util.concurrent.{ConcurrentHashMap, CountDownLatch, TimeUnit}
+import java.util.concurrent._
 
 import com.github.benmanes.caffeine.cache._
 import com.yammer.metrics.core.Meter
+import kafka.metrics.KafkaMetricsGroup
+import kafka.network._
+import kafka.utils._
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.internals.FatalExitError
 import org.apache.kafka.common.utils.{KafkaThread, Time}
@@ -202,6 +201,21 @@ class ProducerStats(producerCacheMaxSize: Int, producerCacheExpiryMs: Long) exte
     .initialCapacity(producerCacheMaxSize/2)
     .removalListener(removalListener)
     .build(cacheLoader)
+
+  // clears cache every 5 mins as caffiene cache does not guarantee to remove entries as soon as expired.
+  // This gives deterministic behavior about producers considered to be inactive as those metrics are removed.
+  Executors.newScheduledThreadPool(1, new ThreadFactory {
+    override def newThread(r: Runnable): Thread = {
+      val thread = Executors.defaultThreadFactory().newThread(r)
+      thread.setName("client-topic-metrics-cache-cleanup-thread")
+      thread.setDaemon(true)
+      thread
+    }
+  }).schedule(new Runnable {
+    override def run(): Unit = {
+      clientTopicPartitions.cleanUp()
+    }
+  }, 5, TimeUnit.MINUTES)
 
   def clientMetrics(clientId:String, topicPartition: TopicPartition): BrokerClientMetrics = {
     Objects.requireNonNull(topicPartition, "topicPartition can not be null")
